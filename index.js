7671df56dbb21161783c197e7df3aab7aadc5b29
ThanhVu121198 <Thanhvu121198@gmail.com>
2021-09-13 15:23:38 +0700

    home

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ var express = require("express");
 var app = express();
 var port = process.env.PORT || 5000;
 app.listen(port,function (){
-    console.log("Sever is running....");
+    console.log("sever is running....");
 });
 app.use(express.static("public"));
 
+//------------ THANH--------------//
+
+app.get("/home",function (req,res) {
+    res.sendFile(__dirname+"/public/home.html")
+});
